4f4fb88e2e5fd4d9540f9ac8b50bf255faa66997
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-02-27 23:37:59 +0530

    Time: 1 ms (99.79%), Space: 48 MB (22.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,22 +3,21 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-
-        dp[0][0] = grid[0][0];
-
-        for(int j=1;j<n;j++){
-            dp[0][j] = dp[0][j-1] + grid[0][j];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
         }
-        for(int i=1;i<m;i++){
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
+        return moves(grid,m-1,n-1,dp);
+    }
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = Math.min(dp[i][j-1],dp[i-1][j]) + grid[i][j];
-            }
-        }
-        return dp[m-1][n-1];
+    public int moves(int[][] grid,int i,int j,int[][] dp){
+        if(i==0 && j==0)return grid[0][0];
+        if(i<0 || j<0)return Integer.MAX_VALUE;
+        if(dp[i][j]!=-1)return dp[i][j];
 
+        int top = moves(grid,i-1,j,dp);
+        int left = moves(grid,i,j-1,dp);
+
+        dp[i][j] = grid[i][j]+Math.min(top,left);
+        return dp[i][j];
     }
 }
