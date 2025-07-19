48ddea2f7708a29c6e825f7f31d20f911d06cced
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-07-19 22:02:08 +0530

    Time: 94 ms (21.32%), Space: 57.7 MB (22.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,28 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+        int sum=0;
         for(int n:nums){
             sum = sum + n;
         }
-        if(sum%2==1){
+        if(sum%2!=0){
             return false;
         }
-            int k = sum/2;
-            Boolean[][] dp = new Boolean[nums.length][k+1];
-            // for(boolean d[]:dp){
-            //     Arrays.fill(d,0);
-            // }
-        
+        int k = sum/2;
+        Boolean[][] dp = new Boolean[nums.length+1][k+1];
         return partition(nums,k,nums.length-1,dp);
     }
-    public boolean partition(int[] nums,int target,int idx,Boolean[][] dp){
-        if(target==0)return true;
-        if(idx==0)return nums[0]==target;
-        if(dp[idx][target] != null) return dp[idx][target];
-        
-        boolean nottake = partition(nums,target,idx-1,dp);
-        boolean take=false;
+    public boolean partition(int[] nums,int k,int idx,Boolean[][] dp){
+        if(k==0)return true;
+        if(idx==0)return nums[0] == k;
+        if(dp[idx][k] != null)return dp[idx][k];
 
-        if(nums[idx]<=target){
-            take = partition(nums,target-nums[idx],idx-1,dp);
+        boolean notaken = partition(nums,k,idx-1,dp);
+        boolean take = false;  
+
+        if(nums[idx]<=k){
+            take = partition(nums,k-nums[idx],idx-1,dp);
         }
-        dp[idx][target] = take || nottake;
-        return dp[idx][target];
+        dp[idx][k] = notaken||take;
+        return dp[idx][k];
     }
 }
