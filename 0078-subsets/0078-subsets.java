1003e1d2d63dae010713b220aadbed84b5dd1c9d
VivekShahare04 <138312154+VivekShahare04@users.noreply.github.com>
2025-10-08 01:27:26 +0530

    Time: 6 ms (1.51%), Space: 43.6 MB (5.36%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,50 @@
 class Solution {
+    HashSet<HashSet<Integer>> ans=new HashSet<>();
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        bt(0,nums,new ArrayList<>(),res);
-        return res;
+        // List<List<Integer>> res = new ArrayList<>();
+        // boolean[] visited = new boolean[nums.length];
+        // solve(nums,visited,new ArrayList<>(),res);
+        // return res;
+        ans.clear();
+        HashSet<Integer> cur=new HashSet<>();
+         
+       
+        int i=0;
+        sol(nums,i,cur);
+          List<List<Integer>> listOfLists = new ArrayList<>();
+
+        for (HashSet<Integer> innerSet : ans) {
+            listOfLists.add(new ArrayList<>(innerSet));
+        }
+        return listOfLists;
+
+        
+       
+        
     }
-    void bt(int index,int[] nums,List<Integer> ls,List<List<Integer>> res){
+    public static void solve(int[] nums,boolean[] visited,List<Integer> ls,List<List<Integer>> res){
         res.add(new ArrayList<>(ls));
 
-        for(int i=index;i<nums.length;i++){
-            //choose fiorst ele
-            ls.add(nums[i]);
-            //find all possible subset according to it
-            bt(i+1,nums,ls,res);
-            //remove last unchoose one
-            ls.remove(ls.size()-1);
+        for(int i=0;i<nums.length;i++){
+            if(!visited[nums[i]]){
+                solve(nums,visited,ls,res);
+                res.add(ls);
+                res.remove(res.size()-1);
+            }
         }
     }
+    public void sol(int[] nums,int i,HashSet<Integer> cur){
+        if(i==nums.length){
+             ans.add(new HashSet<>(cur));
+            return;
+        }
+      
+      
+       sol(nums,i+1,cur);
+       cur.add(nums[i]);
+       sol(nums,i+1,cur);
+       cur.remove(nums[i]);
+      
+    }
+
 }
